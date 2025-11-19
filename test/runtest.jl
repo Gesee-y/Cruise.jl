9eaa9f32894a56364f63e48f8e6443caa975bc6b
Talom Laël <gesee37@gmail.com>
2025-11-19 17:05:38 +0100

    Update runtest.jl

--- test/runtest.jl
+++ test/runtest.jl
@@ -1,12 +1,9 @@
-include(joinpath("..", "src", "Cruise.jl"))
-
-using .Cruise
-#enable_plugin()
-
+using Cruise
 using Test
 
 include("test_temp_storage.jl")
 include("test_cr_subject.jl")
 include("test_gameloop.jl")
+
 
 ## HelloCruise!!()
